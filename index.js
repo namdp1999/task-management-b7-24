51bd224430c00a17ca85ca4163c97f08a4db4204
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-10-30 20:34:47 +0700

    9. Cài đặt CORS

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require('dotenv').config();
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
@@ -8,6 +9,16 @@ const databse = require("./config/database");
 databse.connect();
 
 const routeClient = require("./routes/client/index.route");
+
+// // Tất cả tên miền được phép truy cập vào
+app.use(cors());
+
+// Cho phép 1 tên miền cụ thể được phép truy cập
+// const corsOptions = {
+//   origin: 'http://example.com',
+//   optionsSuccessStatus: 200
+// }
+// cors(corsOptions);
 
 // parse application/json
 app.use(bodyParser.json());
