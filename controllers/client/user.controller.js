a37267d2bed3d4c08ac2ecb5ac82e27a2d934da5
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-10-30 20:55:30 +0700

    11. Tính năng đăng ký

---
+++ controllers/client/user.controller.js
@@ -0,0 +1,37 @@
+const User = require("../../models/user.model");
+const md5 = require("md5");
+
+const generateHelper = require("../../helpers/generate.helper");
+
+module.exports.register = async (req, res) => {
+  const user = req.body;
+
+  const existUser = await User.findOne({
+    email: user.email,
+    deleted: false
+  });
+
+  if(existUser) {
+    res.json({
+      code: "error",
+      message: "Email đã tồn tại trong hệ thống!"
+    });
+    return;
+  }
+
+  const dataUser = {
+    fullName: user.fullName,
+    email: user.email,
+    password: md5(user.password),
+    token: generateHelper.generateRandomString(30)
+  };
+
+  const newUser = new User(dataUser);
+  await newUser.save();
+
+  res.json({
+    code: "success",
+    message: "Đăng ký thành công!",
+    token: newUser.token
+  })
+}
